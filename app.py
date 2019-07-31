efa5c38295ea7e1db5e0d722bc629950bafd0605
SmirnovaAlina <alinas008@gmail.com>
2019-07-31 11:01:43 -0700

    add more models to flask, display results to the front-end

--- app.py
+++ app.py
@@ -4,11 +4,22 @@ import pandas as pd
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+import numpy as np
 import joblib
 import nltk
 # from flask_cors import CORS
 
 app = Flask(__name__)
+
+def reviewPredictor(to_predict_list):
+    to_predict = np.array(to_predict_list)
+    svmmodel = open('svm_model.pkl','rb')
+    clf = joblib.load(svmmodel)
+    vectorizer =joblib.load('vectorizer.pkl')
+    reviews_new = vectorizer.transform(to_predict.ravel())         
+    result = clf.predict(reviews_new)
+    return result[0]
 
 
 @app.route('/')
@@ -31,14 +42,36 @@ def predict():
        
         message = request.form['message']
         data = [message]
-        tf_idf_vect = review_vzer.transform(data)            
-        prediction_dict = dict()
+        # Naive_Bayes model
+        tf_idf_vect = review_vzer.transform(data)  
+        list_for_dict =[]          
+        prediction_dict_NB = dict()
         my_prediction = clf.predict(tf_idf_vect)
         # my_prediction = my_prediction.tolist()[0]
-        prediction_dict['Naive_Bayes'] = my_prediction.tolist()[0]
-       
-    return render_template('result.html', prediction = prediction_dict)
-    # return jsonify(prediction_dict)
+        prediction_dict_NB['model'] = "Naive Bayes"
+        prediction_dict_NB['prediction'] = my_prediction.tolist()[0]
+        list_for_dict.append(prediction_dict_NB)
+
+        # SVM_model 
+        prediction_dict_SVM = dict()
+        result = reviewPredictor(data)
+        if (result)==2:
+        #    prediction='Prediction is positive'
+            prediction_dict_SVM['model'] = "Support Vector Machine"
+            prediction_dict_SVM['prediction'] = "positive"
+        elif (result)==1:
+        #    prediction='Prediction is neutral'
+            prediction_dict_SVM['model'] = "Support Vector Machine"
+            prediction_dict_SVM['prediction'] = "neutral"        
+        else:
+        #    prediction='Prediction is negative' 
+            prediction_dict_SVM['model'] = "Support Vector Machine"
+            prediction_dict_SVM['prediction'] = "negative" 
+        
+        list_for_dict.append(prediction_dict_SVM)
+
+    return render_template('result.html', prediction = list_for_dict, text = message)
+    # return jsonify(list_for_dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
